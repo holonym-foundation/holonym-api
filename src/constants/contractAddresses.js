de21b306f0958d72fdb29f85588b0d90d97c6f89
calebtuttle <1calebtuttle@gmail.com>
2022-12-11 19:33:16 -0500

    Add optimism mainnet contract addresses

--- src/constants/contractAddresses.js
+++ src/constants/contractAddresses.js
@@ -7,25 +7,33 @@ export default {
   //   MerkleTree: "0xa4f7a6E560aE140CD295bf73737eaBf147e4c92D",
   // },
   Hub: {
-    mainnet: {},
+    mainnet: {
+      optimism: "0x87b6e03b0D57771940D7cC9E92531B6217364B3E",
+    },
     testnet: {
       "optimism-goerli": "0x3De3A402E7dD76A03158312115d78a7174443d54",
     },
   },
   IsUSResident: {
-    mainnet: {},
+    mainnet: {
+      optimism: "0x6A78dF871291627C5470F7a768745C3ff05741F2",
+    },
     testnet: {
       "optimism-goerli": "0x1A5f8D110Fa053543184aF404e344a85f5BC6335",
     },
   },
   SybilResistance: {
-    mainnet: {},
+    mainnet: {
+      optimism: "0x3497556f7D0bF602D4237Ecb8ae92840D09E4f63",
+    },
     testnet: {
       "optimism-goerli": "0xBA8a4C5c1f36Dc802d51FEEfF3aa4ef97Dae4B10",
     },
   },
   MerkleTree: {
-    mainnet: {},
+    mainnet: {
+      optimism: "0x56A14Abe1DF94aF0dE78AEF3BD96ae9928D3b415",
+    },
     testnet: {
       "optimism-goerli": "0xF5b00c8681c2B0a5966b2C99dA8FE725e7b90F63",
     },
