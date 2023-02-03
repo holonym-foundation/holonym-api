89d3cd0b70777e876f90017ff26669eb888b1380
calebtuttle <1calebtuttle@gmail.com>
2023-02-02 21:25:08 -0500

    Update contract addresses

--- src/constants/contractAddresses.js
+++ src/constants/contractAddresses.js
@@ -15,19 +15,35 @@ const addrsByNameNetTypeNetwork = {
     },
   },
   IsUSResident: {
+    // v1
+    // mainnet: {
+    //   optimism: "0x6A78dF871291627C5470F7a768745C3ff05741F2",
+    // },
+    // testnet: {
+    //   "optimism-goerli": "0xa6e547FD04C2457Fc37354A9e751aAE4d63949cC",
+    // },
+    // v2
     mainnet: {
-      optimism: "0x6A78dF871291627C5470F7a768745C3ff05741F2",
+      optimism: "0x7497636F5E657e1E7Ea2e851cDc8649487dF3aab",
     },
     testnet: {
-      "optimism-goerli": "0xa6e547FD04C2457Fc37354A9e751aAE4d63949cC",
+      "optimism-goerli": "0xDDDe70439c4C147BF630c8e63aFE5119B630cf64",
     },
   },
   SybilResistance: {
+    // v1
+    // mainnet: {
+    //   optimism: "0x3497556f7D0bF602D4237Ecb8ae92840D09E4f63",
+    // },
+    // testnet: {
+    //   "optimism-goerli": "0x2A817705E2c42Ae320f84AE5F799fdd7044e6B41",
+    // },
+    // v2
     mainnet: {
-      optimism: "0x3497556f7D0bF602D4237Ecb8ae92840D09E4f63",
+      optimism: "0xdD748977BAb5782625AF1466F4C5F02Eb92Fce31",
     },
     testnet: {
-      "optimism-goerli": "0x2A817705E2c42Ae320f84AE5F799fdd7044e6B41",
+      "optimism-goerli": "0x990e6E35432290b3729B659c9ff9748e97F55785",
     },
   },
   MerkleTree: {
