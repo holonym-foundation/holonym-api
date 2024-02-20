499b022e3dce2fc5a9a697558a67989f15275c4c
calebtuttle <1calebtuttle@gmail.com>
2024-02-20 17:49:12 -0500

    update v3 phone circuit constants

--- src/constants/misc.js
+++ src/constants/misc.js
@@ -6,7 +6,13 @@ export const hubV3Chain = 10;
 export const govIdIssuerAddress =
   "0x03fae82f38bf01d9799d57fdda64fad4ac44e4c2c2f16c5bf8e1873d0a3e1993";
 export const phoneIssuerAddress =
-  "0x2998cab3d07a64315f1e8399ecef60a19f478231663f8740703bd30a42a91ed4";
+  "0x40b8810cbaed9647b54d18cc98b720e1e8876be5d8e7089d3c079fc61c30a4";
+// export const phoneIssuerAddress =
+//   process.env.NODE_ENV === "production"
+//     ? "0x40b8810cbaed9647b54d18cc98b720e1e8876be5d8e7089d3c079fc61c30a4"
+//     : "0x2998cab3d07a64315f1e8399ecef60a19f478231663f8740703bd30a42a91ed4";
 
 export const v3KYCSybilResistanceCircuitId =
   "0x729d660e1c02e4e419745e617d643f897a538673ccf1051e093bbfa58b0a120b";
+export const v3PhoneSybilResistanceCircuitId =
+  "0xbce052cf723dca06a21bd3cf838bc518931730fb3db7859fc9cc86f0d5483495";
